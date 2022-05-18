b2244d0a1011ab731f0e9e7afac904cc59572914
rbenitec <105337433+rbenitec@users.noreply.github.com>
2022-05-18 18:10:53 -0500

    modificate

---
+++ src/test/java/NTTDATA/msoperation/MsoperationApplicationTests.java
@@ -0,0 +1,14 @@
+package NTTDATA.msoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+class MsoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
